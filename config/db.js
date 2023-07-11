27464a046055f68fd612bc8df5d15f8a17927a64
Tejas <joharapurkartejas@gmail.com>
2023-07-11 12:10:08 +0530

    changed env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://Tejas:T.e.j.a.s.@nodeandexpressproject.nz4re1q.mongodb.net/Ecommerce?retryWrites=true&w=majority");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(
       `Conneted To Mongodb Databse`
     );
